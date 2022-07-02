c11c613cdde31e143a089843d0ef2939300e4a6b
Akmal Izuddin <izuddin0407@gmail.com>
2022-07-02 12:08:34 +0800

    successfully connected the app to the API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [ postData, setPostData ] = useState([]);
+
+  const getPostData = async () => {
+    const link = 'http://localhost:4000/posts';
+
+    try {
+      const response = await fetch( link, { method: 'GET', mode: 'cors' });
+      const data = await response.json();
+      setPostData( data.postList );
+    }
+    catch( err ) {
+      console.log( err );
+    }
+  }
+
+  useEffect( () => {
+    if( postData.length === 0 ) {
+      getPostData();
+    }
+  }, [ postData ]);
+
   return (
     <div>
-      hello world
+      <div>
+        Hello World
+      </div>
+      <div>
+        {
+          postData.map( ( post ) => {
+            return <div key={ post._id }>{ post.title }</div>
+          })
+        }
+      </div>
     </div>
   );
-}
+};
 
 export { App };
